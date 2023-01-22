24e5dc73a05eb5810919d5743e8b327b13490a4d
hhhhherere <123280191+hhhhherere@users.noreply.github.com>
2023-01-22 17:35:24 -0600

    gg

--- watch time bot/main.py
+++ watch time bot/main.py
@@ -1,8 +1,8 @@
 import time
 from selenium import webdriver
 
-video_link = 'https://www.youtube.com/watch?v=uloK1HOTL1c'
-views = 100
+video_link = 'https://www.youtube.com/watch?v=TnSz9EguKPA'
+views = 1000000
 video_duration = 7 * 60
 
 driver = webdriver.Chrome()
@@ -10,4 +10,4 @@ driver.get(video_link)
 
 for i in range(views):
     time.sleep(video_duration)
-    driver.refresh()
+    driver.refresh(1)
